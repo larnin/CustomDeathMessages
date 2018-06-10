74b2c7d6b198a0ce9d04f23f88954a314633a934
Nicolas <nicolas-donald.laurent@laposte.net>
2018-06-10 02:41:48 +0200

    Messages on death, finish, not having the level and stunt multiplier

--- CustomDeathMessages/Entry.cs
+++ CustomDeathMessages/Entry.cs
@@ -7,17 +7,148 @@ using UnityEngine;
 using System;
 using Spectrum.API.Configuration;
 using System.IO;
+using Harmony;
+using System.Reflection;
+using Events.Car;
+using Events.LocalClient;
+using Events;
+using Events.Stunt;
 
 namespace CustomDeathMessages
 {
     public class Entry : IPlugin
     {
-        public string IPCIdentifier { get { return "Auto Server"; }  set { } }
+        public string IPCIdentifier { get { return "CustomDeathMessages"; }  set { } }
 
         public void Initialize(IManager manager)
         {
-		
+            var harmony = HarmonyInstance.Create("com.Larnin.CustomDeathMessages");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
         }
+
+        [HarmonyPatch(typeof(ChatLog), "OnCarEventDeath")]
+        internal class ChatLogOnCarEventDeath
+        {
+            static bool Prefix(GameObject sender, Death.Data data)
+            {
+                string text = null;
+                switch (data.causeOfDeath)
+                {
+                    case Death.Cause.KillGrid:
+                        text = Message.GetMessage("KillGrid", Message.GetPlayerName());
+                        break;
+                    case Death.Cause.SelfTermination:
+                        text = Message.GetMessage("SelfTermination", Message.GetPlayerName());
+                        break;
+                    case Death.Cause.LaserOverheated:
+                        text = Message.GetMessage("LaserOverheated", Message.GetPlayerName());
+                        break;
+                    case Death.Cause.Impact:
+                    case Death.Cause.ImpactSquish:
+                        text = Message.GetMessage("Impact", Message.GetPlayerName());
+                        break;
+                    case Death.Cause.Overheated:
+                        text = Message.GetMessage("Overheated", Message.GetPlayerName());
+                        break;
+                    case Death.Cause.AntiTunnelSquish:
+                        text = Message.GetMessage("AntiTunnelSquish", Message.GetPlayerName());
+                        break;
+                }
+
+                if (text != null && sender.HasComponent<PlayerDataLocal>())
+                    Message.SendMessage(text);
+
+                return false;
+            }
+        }
+
+        [HarmonyPatch(typeof(ClientLogic), "BroadcastNotEnoughDataToUpdateWorkshopLevel")]
+        internal class ClientLogicBroadcastNotEnoughDataToUpdateWorkshopLevel
+        {
+            static bool Prefix()
+            {
+                Message.SendMessage(Message.GetMessage("KickNoLevel", Message.GetPlayerName()));
+                StaticEvent<NotEnoughInfoToUpdateWorkshopLevel.Data>.Broadcast(null);
+                return false;
+            }
+        }
+
+        [HarmonyPatch(typeof(TimeBasedMode), "LocalPlayerFinishVirtual")]
+        internal class TimeBasedModeLocalPlayerFinishVirtual
+        {
+            static bool Prefix(TimeBasedMode __instance, ref int __result, PlayerDataLocal playerData, FinishType finishType)
+            {
+                int num = -1;
+
+                var list = __instance.GetType().GetField("modePlayerInfos_", BindingFlags.Instance | BindingFlags.NonPublic).GetValue(__instance) as List<ModePlayerInfoBase>;
+
+                TimeBasedModePlayerInfo timeBasedModePlayerInfo = list[playerData.PlayerIndex_] as TimeBasedModePlayerInfo;
+                if (finishType == FinishType.Normal)
+                {
+                    double time = (double)(Timex.ModeTimeCS_ * 10);
+                    double points = (double)playerData.Points_;
+                    num = (int)__instance.EvaluateTimeForMedal(time, points);
+                    if (!CheatsManager.GameplayCheatsUsedThisLevel_)
+                    {
+                        var function = __instance.GetType().GetMethod("UploadScoreAndReplay", BindingFlags.Instance | BindingFlags.NonPublic);
+                        function.Invoke(__instance, new object[] { playerData, finishType, num });
+                    }
+                    Message.SendMessage(Message.GetMessage("Finished", Message.GetPlayerName()));
+                }
+                else if (finishType == FinishType.DNF || finishType == FinishType.Spectate || finishType == FinishType.LeavingLevel)
+                {
+                    num = (int)timeBasedModePlayerInfo.distanceToFinish_;
+                }
+                else if (finishType.IsFinishedAtStart())
+                {
+                    num = -1;
+                }
+                else
+                {
+                    Debug.LogError("Player finished with unknown FinishType: " + finishType);
+                }
+                playerData.CarCamera_.Spectate();
+                __result = num;
+
+                return false;
+            }
+        }
+
+        [HarmonyPatch(typeof(StuntCollectibleLogic), "OnTriggerEnter")]
+        internal class StuntCollectibleLogicOnTriggerEnter
+        {
+            static bool Prefix(StuntCollectibleLogic __instance, Collider other)
+            {
+                bool spawned = (bool)(__instance.GetType().GetField("spawned_", BindingFlags.Instance | BindingFlags.NonPublic).GetValue(__instance));
+                int type = (int)(__instance.GetType().GetField("type_", BindingFlags.Instance | BindingFlags.NonPublic).GetValue(__instance));
+                Phantom phatom = __instance.GetType().GetField("phantom_", BindingFlags.Instance | BindingFlags.NonPublic).GetValue(__instance) as Phantom;
+
+                if (!spawned)
+                {
+                    return false;
+                }
+                PlayerDataLocal playerDataLocal = GUtils.IsRelevantLocalCar(other);
+                if (playerDataLocal)
+                {
+                    MeshRenderer component = __instance.collectibles_[type].GetComponent<MeshRenderer>();
+                    ColorHSB colorHSB = component.sharedMaterial.GetColor("_EmitColor").ToColorHSB();
+                    colorHSB.b = 1f;
+                    Color col = colorHSB.ToColor();
+                    Vector3 collectibleColor = col.ToVector3();
+#pragma warning disable CS0618 // Le type ou le membre est obsolète
+                    StaticTransceivedEvent<HitTagStuntCollectible.Data>.Broadcast(new HitTagStuntCollectible.Data(playerDataLocal.PlayerIndex_, type, collectibleColor));
+#pragma warning restore CS0618 // Le type ou le membre est obsolète
+                    Message.SendMessage(Message.GetMessage("StuntCollect", Message.GetPlayerName(), type + 2));
+                    for (int i = 0; i < __instance.particlesGPU_.Length; i++)
+                    {
+                        __instance.particlesGPU_[i].ZEventReceived(false);
+                        __instance.particlesGPU_[i].ZEventReceived(true);
+                    }
+                    phatom.Play("StuntCollectibleHit", 0f, false);
+                }
+
+                return false;
+            }
         }
     }
 }
---
+++ CustomDeathMessages/Message.cs
@@ -0,0 +1,77 @@
+﻿using Events;
+using Events.ClientToAllClients;
+using Spectrum.API.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace CustomDeathMessages
+{
+    static class Message
+    {
+        static Dictionary<string, string> m_sentenses;
+
+        public static void SendMessage(string message)
+        {
+#pragma warning disable CS0618 // Le type ou le membre est obsolète
+            StaticTransceivedEvent<ChatMessage.Data>.Broadcast(new ChatMessage.Data(message.Colorize("[FFE999]")));
+#pragma warning restore CS0618 // Le type ou le membre est obsolète
+        }
+
+        public static string GetMessage(string key, params object[] values)
+        {
+            if (m_sentenses == null)
+                InitializeSentenses();
+
+            if(!m_sentenses.ContainsKey(key))
+            {
+                throw new Exception("The value you want to retrieve does not exist.");
+            }
+
+            var str = m_sentenses[key];
+            
+            for(int i = 0; i < values.Length; i++)
+                str = str.Replace("{" + i + "}", values[i].ToString());
+
+            return str;
+        }
+
+        public static string GetPlayerName()
+        {
+            var methode = G.Sys.GameManager_.Mode_.GetType().GetMethod("GetSortedListOfModeInfos", BindingFlags.Instance | BindingFlags.NonPublic);
+
+            var clientLogic = G.Sys.PlayerManager_.GetType().GetField("clientLogic_", BindingFlags.Instance | BindingFlags.NonPublic).GetValue(G.Sys.PlayerManager_);
+            return clientLogic.GetType().GetMethod("GetLocalChatName", BindingFlags.Instance | BindingFlags.NonPublic).Invoke(clientLogic, new Object[] { false, "FFFFFF" }) as string;
+        }
+
+        private static void InitializeSentenses()
+        {
+            var settings = new Settings("CustomDeathMessages");
+
+            var entries = new Dictionary<string, string>
+            {
+                {"KillGrid", "{0} was terminated by the laser grid" },
+                {"SelfTermination", "{0} reset" },
+                {"LaserOverheated", "{0} was wrecked after getting split" },
+                {"Impact", "{0} got wrecked" },
+                {"Overheated", "{0} exploded from overheating" },
+                {"AntiTunnelSquish", "{0} got wrecked?" },
+                {"StuntCollect", "{0} grabbed the x{1} multiplier!" },
+                {"KickNoLevel", "{0} was kicked due to not having this level" },
+                {"Finished", "{0} finished" }
+            };
+
+            foreach (var s in entries)
+                if (!settings.ContainsKey(s.Key))
+                    settings.Add(s.Key, s.Value);
+
+            settings.Save();
+
+            m_sentenses = new Dictionary<string, string>();
+            foreach (var s in entries)
+                m_sentenses[s.Key] = settings.GetItem<string>(s.Key);
+        }
+    }
+}
