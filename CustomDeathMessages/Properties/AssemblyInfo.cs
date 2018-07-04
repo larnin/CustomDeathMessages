8bf43bdf4b22efdfb2df7845426243ce340b0881
Nicolas <nicolas-donald.laurent@laposte.net>
2018-07-04 19:11:02 +0200

    Small name changes

--- CustomDeathMessages/Properties/AssemblyInfo.cs
+++ CustomDeathMessages/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Spectrum.Plugins.AutoServer")]
+[assembly: AssemblyTitle("Spectrum.Plugins.CustomDeathMessages")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Spectrum.Plugins.AutoServer")]
+[assembly: AssemblyProduct("Spectrum.Plugins.CustomDeathMessages")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
